02cc80ddf0b677e83bef877dd42fe231cacc536a
Leon Omollo <leonofaugust@gmail.com>
2023-03-26 21:19:35 +0200

    in the every api, removed the conditional '?', also commented out the productApi

--- routes/api/productApi.js
+++ routes/api/productApi.js
@@ -1,35 +1,35 @@
-const express = require('express')
-const router = express.Router()
-const productController = require('../../controllers/productControllers')
-const productModel = require('../../models/schemas/Product')
+// const express = require('express')
+// const router = express.Router()
+// const productController = require('../../controllers/productControllers')
+// const productModel = require('../../models/schemas/Product')
 
-const verifMid = async (req, res, next)=>{
-    const tbu = await productModel.findOne({_id: req.body.prodId})
-    if(!tbu) return res.status(400).json({ "message": `no product with id: ${req.body.updates.prodID} in DB`})
-    if(!tbu.sellerId === req.id) return res.status(401).json({"message":"you can't edit someone else's products"})
-    next()
-}
+// const verifMid = async (req, res, next)=>{
+//     const tbu = await productModel.findOne({_id: req.body.prodId})
+//     if(!tbu) return res.status(400).json({ "message": `no product with id: ${req.body.updates.prodID} in DB`})
+//     if(!tbu.sellerId === req.id) return res.status(401).json({"message":"you can't edit someone else's products"})
+//     next()
+// }
 
-router.route('/')
-    .post(productController.createProduct)
-    .get(productController.getAllProducts)
-    .put(verifMid, productController.updateProduct)
-    .delete(verifMid, productController.deleteProduct);
+// router.route('/')
+//     .post(productController.createProduct)
+//     .get(productController.getAllProducts)
+//     .put(verifMid, productController.updateProduct)
+//     .delete(verifMid, productController.deleteProduct);
     
-router.route('/aspect')
-    .get(productController.getFiltered)
-router.route('/find/:daiquiri')
-    .get(productController.findProduct)
-router.route('/like')
-    .patch(productController.likeProduct);
-router.route('/unlike')
-    .patch(productController.unlikeProduct);
-router.route('/feed')
-    .get(productController.populateFeed);
-router.route('/u/:userId')
-    .get(productController.getUserProds)
-router.route('/:prodID')
-    .get(productController.getProduct);
+// router.route('/aspect')
+//     .get(productController.getFiltered)
+// router.route('/find/:daiquiri')
+//     .get(productController.findProduct)
+// router.route('/like')
+//     .patch(productController.likeProduct);
+// router.route('/unlike')
+//     .patch(productController.unlikeProduct);
+// router.route('/feed')
+//     .get(productController.populateFeed);
+// router.route('/u/:userId')
+//     .get(productController.getUserProds)
+// router.route('/:prodID')
+//     .get(productController.getProduct);
 
 
-module.exports = router
+// module.exports = router
--- routes/api/userApi.js
+++ routes/api/userApi.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken');
 
 const jwtForUpdateDelete = (req, res, next)=>{
     const authHeader = req.headers.authorization || req.headers.Authorization;
-    if (!authHeader?.startsWith('Bearer ')) return res.sendStatus(401);
+    if (!authHeader.startsWith('Bearer ')) return res.sendStatus(401);
     const token = authHeader.split(' ')[1];
     
     jwt.verify(
